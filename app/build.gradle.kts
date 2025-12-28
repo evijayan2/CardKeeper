95f9ece0a37172c6cd5c3690ca0aa0a8e723bf0a
Vijay <evijayanathan@gmail.com>
2025-12-27 22:48:27 -0500

    chore: release v1.0.0-alpha.4 - Performance, Security & Schema Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vijay.cardkeeper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0-alpha.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
