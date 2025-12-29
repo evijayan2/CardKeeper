0c8504487578d46fa294ecb82b20f3d074b30ad5
Vijay <evijayanathan@gmail.com>
2025-12-29 12:34:36 -0500

    Bump version to 1.0.0-alpha.6 and add release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vijay.cardkeeper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.0-alpha.5"
+        versionCode = 4
+        versionName = "1.0.0-alpha.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
