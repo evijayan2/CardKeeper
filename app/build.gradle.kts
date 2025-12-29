dc62312ad48e817f33d9bfb6c53be8678a8a6604
Vijay <evijayanathan@gmail.com>
2025-12-29 13:43:22 -0500

    CI: Streamline build and bump to alpha.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vijay.cardkeeper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.0-alpha.11"
+        versionCode = 10
+        versionName = "1.0.0-alpha.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
