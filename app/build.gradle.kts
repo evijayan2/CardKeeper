ea47176c1b1eae34bc91f4b93c47e0cac0430b97
Vijay <evijayanathan@gmail.com>
2025-12-29 13:41:09 -0500

    CI: Final stabilization, increase memory, and bump to alpha.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vijay.cardkeeper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.0.0-alpha.10"
+        versionCode = 9
+        versionName = "1.0.0-alpha.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
