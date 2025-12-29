f311e62dcf97c61c63d891b440df47dd98fdb0f5
Vijay <evijayanathan@gmail.com>
2025-12-29 14:50:05 -0500

    feat: Fix changelog link and automate version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vijay.cardkeeper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.0.0-alpha.17"
+        versionCode = 16
+        versionName = "1.0.0-alpha.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
