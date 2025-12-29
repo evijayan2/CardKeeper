8b184df137919891dc5bf0a30a0cd02c19e76087
Vijay <evijayanathan@gmail.com>
2025-12-29 13:35:38 -0500

    CI: Simplify build and bump to alpha.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vijay.cardkeeper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.0-alpha.9"
+        versionCode = 8
+        versionName = "1.0.0-alpha.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
