927abd8f55dea59d12e3efcff1024d2ab736afd0
Vijay <evijayanathan@gmail.com>
2025-12-29 14:33:58 -0500

    feat: Configure commit-based changelog generation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vijay.cardkeeper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.0.0-alpha.16"
+        versionCode = 15
+        versionName = "1.0.0-alpha.17"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
