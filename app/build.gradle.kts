1cfa09a358ed1d63b0496fa1dbcde0243b747d75
Vijay <evijayanathan@gmail.com>
2025-12-29 12:14:40 -0500

    build: Configure release build type to use debug signing configuration.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
