6c162fbdea9977dc77e258d8e35bfab8b1d4b9bd
Vijay <evijayanathan@gmail.com>
2025-12-29 13:34:03 -0500

    CI: Debug hang with --info and bump to alpha.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vijay.cardkeeper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.0-alpha.8"
+        versionCode = 7
+        versionName = "1.0.0-alpha.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
