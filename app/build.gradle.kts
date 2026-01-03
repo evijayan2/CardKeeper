a62ec4026623ad259628cb4a91bb7446cfef10a0
Vijay <evijayanathan@gmail.com>
2026-01-02 23:04:45 -0500

    chore: bump version to 1.0.0-alpha.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.vijay.cardkeeper"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.0.0-alpha.18"
+        versionCode = 17
+        versionName = "1.0.0-alpha.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
